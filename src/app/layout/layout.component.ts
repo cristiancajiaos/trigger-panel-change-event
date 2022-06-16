66d54458756208af7cf85e39de4b61973ec9df47
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-06-16 15:17:56 -0400

    Creado acordeón

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  disabled: boolean = false;
 
-  constructor() { }
+  panels = [
+    { id: 'panel-1', title: 'Title 1', content: 'Content 1' },
+    { id: 'panel-2', title: 'Title 2', content: 'Content 2' },
+    { id: 'panel-3', title: 'Title 3', content: 'Content 3' },
+    { id: 'panel-4', title: 'Title 4', content: 'Content 4' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
